ea1c76231e7596fd7d5e6f45645a043b3f9275db
LongDN <longdn@pvcombank.com.vn>
2024-01-08 17:32:47 +0700

    update code

--- db/create-table.sql
+++ db/create-table.sql
@@ -12,11 +12,37 @@
 -- 	(1, 'PENDING', '', now(), NULL),
 -- 	(2, 'ACCEPTED', 'Some notes', now(), now());
 
-CREATE TABLE test1 (
-	id int8 NOT NULL,
-	"name" varchar(255) NOT NULL,
-	CONSTRAINT id_pk_tes1 PRIMARY KEY (id)
+create table test_table_1
+(
+    id           integer generated always as identity
+        primary key,
+    code         varchar(45) default NULL::character varying,
+    name         varchar(45) default NULL::character varying,
+    created_date timestamp   default CURRENT_TIMESTAMP,
+    updated_date timestamp   default CURRENT_TIMESTAMP
 );
+
+create table test_table_2
+(
+    id           integer generated always as identity
+        primary key,
+    code         varchar(45) default NULL::character varying,
+    price        double precision,
+    created_date timestamp   default CURRENT_TIMESTAMP,
+    updated_date timestamp   default CURRENT_TIMESTAMP
+);
+
+create trigger t1_updated_at_modtime
+    before update
+    on test_table_1
+    for each row
+execute procedure update_updated_at_column();
+
+create trigger t1_updated_at_modtime
+    before update
+    on test_table_2
+    for each row
+execute procedure update_updated_at_column();
 
 -- CREATE TABLE test2 (
 -- 	id int8 NOT NULL,
